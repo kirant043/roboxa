b80450c303153f1ebcbad6bec0ec392749d1748b
Kiran Tangellapalli <kirant043@gmail.com>
2022-07-08 18:48:11 +0530

    fix

--- server.js
+++ server.js
@@ -10,6 +10,24 @@ const { Server } = require("socket.io");
 const config =  require('./config.js');
 const PORT = config.PORT;
 
+var cors = require('cors'); //import cors module
+var whitelist = ['http://localhost:5000']; //white list consumers
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(null, false);
+    }
+  },
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+};
+
+app.use(cors(corsOptions));
+
 // mongoose for mongodb
 // set the port
 var database = require("./config/database"); // load the database config
