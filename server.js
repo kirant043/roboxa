9b73a0353672dcdda5e5f084c93890622dfffb2b
Kiran Tangellapalli <kirant043@gmail.com>
2022-07-15 19:34:22 +0530

    fix

--- server.js
+++ server.js
@@ -57,6 +57,7 @@ io.sockets.on("connection", function (socket) {
   //console.log("Successfully connected");
   // convenience function to log server messages on the client
   function log() {
+    console.log(arguments);
     var array = ["Message from server:"];
     array.push.apply(array, arguments);
     socket.emit("log", array);
