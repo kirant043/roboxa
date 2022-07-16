b39f09005700be12d152fb28f7d32cc2614d02c2
Kiran Tangellapalli <kirant043@gmail.com>
2022-07-16 11:48:29 +0530

    fix

--- server.js
+++ server.js
@@ -386,7 +386,7 @@ io.sockets.on("connection", function (socket) {
     console.log("leaveGroup");
     console.log(args);
     //log(args);
-    console.log(io.sockets.clients(args.room))
+    console.log(io.of('/').clients(args.room))
     io.sockets.clients(args.room).forEach(function(s){
       s.leave(args.room);
     });
